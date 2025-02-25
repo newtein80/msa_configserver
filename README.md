47dc1a33ffa7b002f15ea1d98f6154ac0de029ba
newtein80 <newtein80@gmail.com>
2025-02-25 15:59:01 +0900

    add sample log

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ```bash
 docker run -d -p 15672:15672 -p 5672:5672 --name rabbitmq rabbitmq
 ```
+
+```bash
+docker exec rabbitmq rabbitmq-plugins enable rabbitmq_management
+```
+
+[access local rabbitmq](http://localhost:15672)
--- src/main/java/com/mobigen/configserver/ConfigserverApplication.java
+++ src/main/java/com/mobigen/configserver/ConfigserverApplication.java
@@ -1,15 +1,27 @@
 package com.mobigen.configserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner() {
+		return (a) -> {
+			log.info("\n" + "START!");
+		};
 	}
 
 }
